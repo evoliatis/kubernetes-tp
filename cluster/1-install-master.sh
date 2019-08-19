28b793ee9d8688ca86d37d713ce60a5bef111ec9
Samuel COLLADO <scollado@evoliatis.com>
2019-08-19 21:24:08 +0200

    Modification pour support 1.2

--- cluster/1-install-master.sh
+++ cluster/1-install-master.sh
@@ -2,6 +2,6 @@
 echo "Mise en place du master"
 echo -n "Adresse IP publique du serveur : "
 read ip
-kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=$ip --kubernetes-version stable-1.9
+kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=$ip --kubernetes-version stable-1.15
 echo "N'oubliez d'ajouter vos minions à l'aide de la commande kubeadm join"
 exit 0
--- cluster/dashboard/install.sh
+++ cluster/dashboard/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Mise en place des composants"
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
 echo "N'oubliez pas de créer le compte utilisateur"
 echo "La connexion se réalise avec kubectl proxy"
 exit 0
